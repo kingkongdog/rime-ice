e68471c1c9b86eec23c72427524d1e1160a6fdd4
ligang <kingkongdog9527@gmail.com>
2026-01-15 17:49:19 +0800

    feat: restore formal code

--- lua/force_digit_input_processor.lua
+++ lua/force_digit_input_processor.lua
@@ -7,8 +7,7 @@ function M.func(key, env)
 
   if code >= 48 and code <= 57 then
     -- 这里不要用 key:repr()，否则可能推入 Shift+1
-    -- context:push_input(string.char(code))
-    context:push_input("1")
+    context:push_input(string.char(code))
     return 1
   end
 
